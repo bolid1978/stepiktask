965f1b4ee049e2dcac78815be44c5b0c60d85148
bolid <bratik.n@yandex.ru>
2022-05-17 14:33:51 +0300

    wood 1.1

---
+++ src/wood/wood.java
@@ -0,0 +1,50 @@
+package wood;
+
+import java.util.TreeSet;
+import java.util.concurrent.ThreadLocalRandom;
+
+//Глубина дерева
+//        Необходимо реализовать функцию:
+//        на вход функция получает root бинарного дерева;
+//        внутри функции нужно вычислить максимальную глубину дерева;
+//        функция должна вернуть значение глубины дерева.
+//        Дерево состоящее всего из одного элемента имеет глубину равной единице.
+//        На вход теста передаются параметры, необходимые для генерации рандомизированного дерева.
+public class wood {
+
+
+
+    public static void main(String[] args) {
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        ThreadLocalRandom  cifra = ThreadLocalRandom.current();
+        for (int i = 0; i < 20; i++) {
+            treeSet.add(cifra.nextInt(0,1000));
+        }
+        System.out.println(treeSet);
+
+
+
+
+    }
+
+    class Node {
+        private Node left;
+        private Node right;
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
+    }
+}
