91a4ef42385f5e7c94360e2146f33341e7afbe35
bolid <bratik.n@yandex.ru>
2022-05-19 16:22:07 +0300

    reverBIt 1.2

--- src/reverseBytes/reverseBytes.java
+++ src/reverseBytes/reverseBytes.java
@@ -8,13 +8,15 @@ public class reverseBytes {
 
         long d = Long.parseLong(str,16);
         System.out.println(d);
+        //long mask = Long.MAX_VALUE - 1;
         long mask = Long.MAX_VALUE - 1;
         for (int i = 0; i < 64 ; i++) {
+
             if((d >> i & 1) == 0)  d = d|(1 << i);
             else{
-                mask<<
+                if(i != 0 )  mask = (mask << i) | 1;
+                d = d & mask;
             }
-
         }
     }
 
