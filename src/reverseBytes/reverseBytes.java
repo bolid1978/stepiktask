de9698c05d6baf28660ac6feadc4e91ec0083f76
bolid <bratik.n@yandex.ru>
2022-05-19 16:11:41 +0300

    reverBIt 1.1

--- src/reverseBytes/reverseBytes.java
+++ src/reverseBytes/reverseBytes.java
@@ -5,8 +5,17 @@ public class reverseBytes {
 
     public static void main(String[] args) {
         String str = "fe01ccd1";
+
         long d = Long.parseLong(str,16);
         System.out.println(d);
+        long mask = Long.MAX_VALUE - 1;
+        for (int i = 0; i < 64 ; i++) {
+            if((d >> i & 1) == 0)  d = d|(1 << i);
+            else{
+                mask<<
+            }
+
+        }
     }
 
 }
