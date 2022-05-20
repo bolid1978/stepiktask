521ba8e79de97ea30d52c98cd36f9b4de35ef33d
bolid <bratik.n@yandex.ru>
2022-05-20 11:37:20 +0300

    reverByte final

--- src/reverseBytes/reverseBytes.java
+++ src/reverseBytes/reverseBytes.java
@@ -4,20 +4,27 @@ public class reverseBytes {
 
 
     public static void main(String[] args) {
-        String str = "fe01ccd1";
-
-        long d = Long.parseLong(str,16);
-        System.out.println(d);
+//        String str = "fe01ccd1";
+//
+//        long d = Long.parseLong(str,16);
+        int a = 0xd1cc01fe;
+        int d = 0xfe01ccd1;
+//        System.out.printf("%H",d);
+//        System.out.println();
+//        System.out.printf("%H",a);
+//        System.out.println();
         //long mask = Long.MAX_VALUE - 1;
-        long mask = Long.MAX_VALUE - 1;
-        for (int i = 0; i < 64 ; i++) {
+        int mask = 0b11111111_11111111_11111111_11111110 ;
+        for (int i = 0; i < 32 ; i++) {
 
             if((d >> i & 1) == 0)  d = d|(1 << i);
             else{
-                if(i != 0 )  mask = (mask << i) | 1;
-                d = d & mask;
-            }
+
+                d = d & mask;}
+            mask = (mask << 1) | 1;
         }
+        //0xd1cc01fe
+        System.out.printf("%H",d);
     }
 
 }
