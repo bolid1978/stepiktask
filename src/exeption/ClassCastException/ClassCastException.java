0e16210fcbdc2dae057930eff94053258b93f437
bolid1978 <95186968+bolid1978@users.noreply.github.com>
2022-05-24 20:00:25 +0300

    exeption

--- src/exeption/ClassCastException/ClassCastException.java
+++ src/exeption/ClassCastException/ClassCastException.java
@@ -13,30 +13,7 @@ public class ClassCastException {
 
     static List<Integer> generateBadList() {
         ArrayList  arrayList = new ArrayList<>();
-       Number number =  new Number() {
-           @Override
-           public int intValue() {
-               return 78;
-           }
-
-           @Override
-           public long longValue() {
-               return 0;
-           }
-
-           @Override
-           public float floatValue() {
-               return 0;
-           }
-
-           @Override
-           public double doubleValue() {
-               return 0;
-           }
-       };
-       arrayList.add(number);
-
-
+        arrayList.add("kg");
         return arrayList;
 
 
